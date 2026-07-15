0fff200e31b32356a8b7950876445c77b85be063
Nicholas-py <nicholas.waslander@gmail.com>
2026-07-14 21:42:58 -0400

    test?

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   tanstackStart: {
     // Redirect TanStack Start's bundled server entry to src/server.ts (our SSR error wrapper).
     // nitro/vite builds from this
-    server: { entry: "server" },
+    server: { entry: "server",preset:"node" },
   },
   vite: {
     base: "/universal/"
