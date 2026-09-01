eca9ca4924a1336ee27a9995881756e4cce6340e
Nicholas-py <nicholas.waslander@gmail.com>
2026-09-01 12:15:00 -0400

    Remove lovable (?)

--- vite.config.ts
+++ vite.config.ts
@@ -4,36 +4,48 @@
 //     componentTagger (dev-only), VITE_* env injection, @ path alias, React/TanStack dedupe,
 //     error logger plugins, and sandbox detection (port/host/strictPort).
 // You can pass additional config via defineConfig({ vite: { ... }, etc... }) if needed.
-import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import { defineConfig } from "vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from '@vitejs/plugin-react'
+import tailwindcss from "@tailwindcss/vite";
 
 import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
-  tanstackStart: {
-    // Redirect TanStack Start's bundled server entry to src/server.ts (our SSR error wrapper).
-    // nitro/vite builds from this
-    server: { entry: "server" },
-  },
-
-  vite: {
-    base: "/universal/"
-  },
+  base: "/universal/",
 
   //For some reason, doesn't load CSS without this. Is not actually an error (I think)
-  nitro: {
-    baseURL: "/universal",
-    preset: "cloudflare-module",
-    cloudflare: {
-      deployConfig: false,
-    },
+  // nitro: {
+  //   baseURL: "/universal",
+  //   preset: "cloudflare-module",
+  //   cloudflare: {
+  //     deployConfig: false,
+  //   },
+  // },
+  // build: {
+  //   outDir: "/universal/assets/"
+  // },
+  css: {
+    transformer: "postcss"
   },
   build: {
-  outDir:  "/universal/assets/"
-},
+    cssMinify: "esbuild",
+  },
+
+  resolve: {
+    tsconfigPaths: true,
+  },
 
   plugins: [cloudflare({
     viteEnvironment: {
       name: "ssr"
     }
-  })]
+  }),
+  tanstackStart({
+    server: { entry: "server" },
+  }),
+  viteReact(),
+  tailwindcss(),
+
+  ]
 });
